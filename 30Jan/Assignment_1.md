3d29af5b6a0400144591b25b92c3a9ed8b3fcb4d
Aditya Iyer <aiyer.aditya@gmail.com>
2019-02-04 22:34:56 +0530

    Answering Questions. Changed to HomeWork Data

--- 30Jan/Assignment_1.md
+++ 30Jan/Assignment_1.md
@@ -4,13 +4,28 @@ We dissected a cockroach leg and looked at baseline and stimulated action potent
 % This is exactly what we did
 
 ## Questions
-* What happens if you choose threshold too low or too high?
+* **What happens if you choose threshold too low or too high?**
 
-* What happens to the firing rate as stimulus strength reduces?
+  Take a look at the figures showing baseline and stimulus response potentials (Figure x). If we consider a threshold too low, there will be too many ![false positives](https://en.wikipedia.org/wiki/False_positives_and_false_negatives), i.e. peaks that are not necessarily action potentials will be counted as spikes.
 
-  * Why is it called rate coding?
+  If the threshold is too high, we could miss out on action potentials. (See Interactive Figure for the same)
 
-  * Why do sensory systems need rate coding?
+* **What happens to the firing rate as stimulus strength reduces?**
+
+  As the stimulus strength is reduced, the firing rate is expected to reduce. Considering our threshold, we see that <>, as indicated in Figure x.
+
+  <div style="text-align:center"><img src="barplot.jpeg" alt="drawing" width="100"/>
+
+  * **Why is it called rate coding?**
+
+    The relationship between the strength of the stimulus and the spikes of evoked action potentials is called rate coding.
+
+    From the neuron's perspective, the strength of the stimulus is being translated to the frequency of spikes. In other words, the strength of the stimulus is being *coded* into the *rate* of spike firing. Hence, this relationship can be aptly called rate coding
+
+  * **Why do sensory systems need rate coding?**
+
+    Consider the following system
+
 
 * Is the mean amplitude same across baseline and stimulus? Why?
 
--- 30Jan/Jan30_UB304L.m
+++ 30Jan/Jan30_UB304L.m
@@ -2,18 +2,18 @@ global voltageData
 global samplingRate
 global action_threshold
 
-[voltageData, samplingRate]= audioread('LabData.wav');
+[voltageData, samplingRate]= audioread('homeWorkData.wav');
 time_diff = 1/samplingRate;
 time_points = time_diff:time_diff:length(voltageData)/samplingRate;
-
+plot(voltageData)
 
 % Visualsing Waveform of the audio file at a much finer scale 
 % Assumption: Highest Voltage will be during stimulation, hence, during
 % An Action Potential
-maximum = max(voltageData);
+maximum = max(voltageData(350*samplingRate:360*samplingRate));
 index = find(voltageData == maximum);
 figure
-plot(voltageData(index-200:index+200))
+plot(voltageData(index-50:index+50))
 title('Waveform of Action Potential')
 ylabel('Voltage(V)')
 xlabel('Time (Undefined)')
@@ -37,38 +37,38 @@ xlabel('Threshold (V)')
 saveas(gcf,strcat('Threshold'),'epsc')
 
 % Defining Threshold for Action Potentials And Show Threshold on Figure
-action_threshold = 0.3;
+action_threshold = 0.5;
 % Plotting Raw Voltage Data as a function of Time
 figure
 plot(time_points, voltageData)
 title('Raw Plot of Voltage vs Time')
 xlabel('Time (s)')
 ylabel('Voltage (V)')
-hline = refline([0 0.3]);
+hline = refline([0 action_threshold]);
 hline.Color = 'red';
 saveas(gcf,strcat('RawPlot'),'epsc')
 
 % The next few lines are for getting the digitized data from timepoints
-baselineL_start = 4;
-baselineL_stop = 12;
-low_start = 25.5;
-low_stop = 39.5;
+baselineL_start = 196;
+baselineL_stop = 206;
+low_start = 240;
+low_stop = 250;
 [spikes_low_baseline, spikes_low, mean_baselineL, mean_low] = plot_data(...
     baselineL_start, baselineL_stop, low_start, low_stop, 'Low');
 
     
-baselineM_start = 79;
-baselineM_stop = 90;
-medium_start = 102;
-medium_stop = 108;
+baselineM_start = 262;
+baselineM_stop = 272;
+medium_start = 308;
+medium_stop = 318;
 [spikes_medium_baseline, spikes_medium, mean_baselineM, mean_medium] = ...
     plot_data(baselineM_start, baselineM_stop, medium_start, medium_stop, 'Medium');
 
 
-baselineH_start = 308;
-baselineH_stop = 315;
-high_start = 325.5;
-high_stop = 344;
+baselineH_start = 326;
+baselineH_stop = 336;
+high_start = 350;
+high_stop = 360;
 [spikes_high_baseline, spikes_high, mean_baselineH, mean_high] = ...
     plot_data(baselineH_start, baselineH_stop, high_start, high_stop, 'High');
 
@@ -93,11 +93,11 @@ set(gca,'XTickLabel',{'Baseline','Low', 'Baseline', 'Medium', 'Baseline', 'High'
 saveas(gcf,strcat('ActionPotentialPeaks'),'epsc')
 
 % For Adaptation
-baseline_start = 394;
-baseline_stop = 404;
+baseline_start = 410;
+baseline_stop = 420;
 [voltage_baseline, time_baseline] = get_voltage(baseline_start, baseline_stop);
-start = 436;
-stop = 456;
+start = 421;
+stop = 451;
 count = 1;
 spikes = (0);
 for s1 = start:1:stop
